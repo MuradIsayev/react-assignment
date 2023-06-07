60eb0175add2c567228aefbbec290dd4c7171904
MurAd <muradisayevazer@gmail.com>
2023-06-07 18:35:51 +0400

    mock data added

---
+++ src/transactionData.js
@@ -0,0 +1,14 @@
+export const transactionData = [
+  { customerId: 1, amount: 120, date: '2023-03-01' },
+  { customerId: 2, amount: 80, date: '2023-03-01' },
+  { customerId: 1, amount: 50, date: '2023-03-10' },
+  { customerId: 2, amount: 150, date: '2023-03-15' },
+  { customerId: 1, amount: 75, date: '2023-04-01' },
+  { customerId: 2, amount: 200, date: '2023-04-05' },
+  { customerId: 1, amount: 100, date: '2023-04-10' },
+  { customerId: 2, amount: 60, date: '2023-04-15' },
+  { customerId: 1, amount: 90, date: '2023-05-01' },
+  { customerId: 2, amount: 100, date: '2023-05-05' },
+  { customerId: 1, amount: 120, date: '2023-05-10' },
+  { customerId: 2, amount: 75, date: '2023-05-15' },
+];
